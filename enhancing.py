45a91a9fd9328885815c0783d5c6dd96b5fc01e0
MichaelYu <michaelyu781@126.com>
2024-05-30 03:47:08 +0800

    Update: Trainer

--- enhancing.py
+++ enhancing.py
@@ -102,6 +102,16 @@ class TRANS(nn.Module):
 
         return clamped_est_trans, mid_loss
 
+class Smooth_Loss(nn.Module):
+    def __init__(self):
+        super(Smooth_Loss, self).__init__()
+
+    def forward(self, x):
+        loss_smooth = torch.mean(torch.abs(x[:, :, :, :-1] - x[:, :, :, 1:])) + \
+                      torch.mean(torch.abs(x[:, :, :-1, :] - x[:, :, 1:, :]))
+
+        return loss_smooth
+
 
 class PRE_DEHAZE_T(nn.Module):
 
@@ -112,7 +122,7 @@ class PRE_DEHAZE_T(nn.Module):
 
         self.trans_net = TRANS(in_channels=img_channels, out_channels=t_channels,
                                      n_resblock=n_resblock, n_feat=n_feat)
-        self.smooth_loss = smooth_loss.Smooth_Loss()
+        self.smooth_loss = Smooth_Loss()
 
     def forward(self, x):
         b, c, h, w = x.size()
@@ -127,6 +137,55 @@ class PRE_DEHAZE_T(nn.Module):
         return output, trans, air, mid_loss
 
 
+class FusionModule(nn.Module):
+    def __init__(self, n_feat, kernel_size=5):
+        super(FusionModule, self).__init__()
+        print("Creating BRB-Fusion-Module")
+        self.block1 = blocks.BinResBlock(n_feat, kernel_size=kernel_size)
+        self.block2 = blocks.BinResBlock(n_feat, kernel_size=kernel_size)
+
+    def forward(self, x, y):
+        H_0 = x + y
+
+        x_1, y_1, H_1 = self.block1(x, y, H_0)
+        x_2, y_2, H_2 = self.block2(x_1, y_1, H_1)
+
+        return H_2
+
+
+class DEHAZE_T(nn.Module):
+
+    def __init__(self, img_channels=3, t_channels=1, n_resblock=3, n_feat=32, device='cuda'):
+        super(DEHAZE_T, self).__init__()
+        print("Creating Dehaze-T Net")
+        self.device = device
+
+        self.extra_feat = nn.Sequential(
+            nn.Conv2d(img_channels, n_feat, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            blocks.ResBlock(n_feat, n_feat, kernel_size=5, stride=1)
+        )
+        self.fusion_feat = FusionModule(n_feat=n_feat, kernel_size=5)
+        self.trans_net = TRANS(in_channels=1, out_channels=t_channels,
+                                     n_resblock=n_resblock, n_feat=n_feat, feat_in=True)
+        self.smooth_loss = Smooth_Loss()
+
+    def forward(self, x, pre_est_J):
+        b, c, h, w = x.size()
+
+        x_feat = self.extra_feat(x)
+        pre_est_J_feat = self.extra_feat(pre_est_J)
+
+        fusioned_feat = self.fusion_feat(x_feat, pre_est_J_feat)
+
+        trans, _ = self.trans_net(fusioned_feat)
+        air = torch.ones(b, 1, h, w).to(self.device)
+
+        output = (1 / trans) * x + ((trans - 1) / trans) * air
+
+        mid_loss = self.smooth_loss(trans)
+
+        return output, trans, air, mid_loss
 
 
 class DEHAZE_SGID_PFF(nn.Module):
